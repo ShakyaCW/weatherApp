70a89230eeffe9405b178ef06647a0c965a7c32c
ShakyaCW <108785724+ShakyaCW@users.noreply.github.com>
2023-06-01 10:04:19 +0530

    Fetch weather data for each city code and store in the cache

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ async function extractCityCodes() {
 
 // Call the extractCityCodes function and store the result in a variable
 const cityCodesPromise = extractCityCodes();
+
+// Fetch weather data for each city code and store in the cache
+cityCodesPromise.then(cityCodes => {
+    cityCodes.forEach(cityCode => {
+      // Create the API URL for each city code
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=${cityCode}&appid=${API_KEY}`;
+  
+      // Fetch the weather data for the city code
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(jsonData => {
+          // Store the data in the cache using city code as the key
+          cache.set(cityCode, jsonData);
+  
+          console.log(`Data fetched and stored for city code ${cityCode}`);
+        })
+        .catch(error => console.error('Error:', error));
+    });
+  });
+  
+  // Access the cityCodes array outside the function
+  cityCodesPromise.then(cityCodes => {
+    console.log(cityCodes);
+  });
+  
+  // Variables for tracking cache status
+  let cachedCounter = 0; // Counter to track the number of cached entries
+  let cachedTime = 0; // Timestamp to track the time of caching
